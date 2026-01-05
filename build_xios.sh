1ef963a02247946e0a58b6077336901ccd588d10
lparisi <l.parisi@epcc.ed.ac.uk>
2026-01-05 11:17:45 +0000

    Added options to build from different branches

--- build_xios.sh
+++ build_xios.sh
@@ -1,25 +1,38 @@
+set -e 
+set -x
 
-OPT="debug"
+OPT="dev"
 COMPILER="GNU"
-
+DOWNLOAD=0
+BRANCH="main"
 
 # load compiler specific modules
-
+set +x # Disable command echoing to limit noise 
 if [ "$COMPILER" = "GNU" ]; then
     module load PrgEnv-gnu
-endif
+fi
 
 
 module load cray-hdf5-parallel
 module load cray-netcdf-hdf5parallel/4.9.0.17
+set -x # Re-enable command echoing after loading modules
 
 
-# Clone XIOS repository
-git clone https://gitlab.in2p3.fr/ipsl/projets/xios-projects/xios.git xios3
+XIOS_DIR=$BRANCH
+if [ $BRANCH = "main" ];
+then
+    XIOS_DIR="xios-3"
+fi
 
-# Set architecture dependent configuration files
-cp -r arch/* xios3/arch/
+# Clone XIOS repository if DOWNLOAD is set
+if [ $DOWNLOAD -eq 1 ];
+then
+    git clone -b $BRANCH https://gitlab.in2p3.fr/ipsl/projets/xios-projects/xios.git $XIOS_DIR
+    # Set architecture dependent configuration files
+    cp -r arch/* $XIOS_DIR/arch/
+fi
+
 
 # Build XIOS
-cd xios3
-./make_xios --debug --arch GCC_CRAY_EX4000 --job 16
+cd $XIOS_DIR
+./make_xios --$OPT --arch GCC_CRAY_EX4000 --job 16
